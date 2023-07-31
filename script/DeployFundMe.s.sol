1cb81e04a5bbbc1a90fc48410705714abd46d6d0
Michael Longoria <michaelandrewlongoria@gmail.com>
2023-07-30 22:51:46 -0700

    Adds README, formats code

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -2,22 +2,20 @@
 
 pragma solidity ^0.8.18;
 
-import {Script} from 'forge-std/Script.sol';
-import {FundMe} from '../src/FundMe.sol';
-import {HelperConfig} from './HelperConfig.s.sol';
+import {Script} from "forge-std/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
+    function run() external returns (FundMe) {
+        // Before startBroadcast => Not a "real" txn
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
 
-  function run() external returns (FundMe) {
-
-    // Before startBroadcast => Not a "real" txn
-    HelperConfig helperConfig = new HelperConfig();
-    address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
-
-    // After startBroadcast => "real" txn
-    vm.startBroadcast();
-    FundMe fundMe = new FundMe(ethUsdPriceFeed);
-    vm.stopBroadcast();
-    return fundMe;
-  }
+        // After startBroadcast => "real" txn
+        vm.startBroadcast();
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
+        vm.stopBroadcast();
+        return fundMe;
+    }
 }
